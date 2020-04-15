061fa8447f39aaecfa5405c8f906b3f376569d47
zqx <2654784320@qq.com>
2020-04-15 18:24:40 +0800

    我

--- 穿搭style/miniprogram/pages/weather/weather.js
+++ 穿搭style/miniprogram/pages/weather/weather.js
@@ -1,66 +1,24 @@
-// pages/weather/weather.js
+var amapFile = require('../../common/amap-wx.js');//如：..­/..­/libs/amap-wx.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    weatherData:''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function () {
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({ key: '1086065bcc087f39ebf8b09ebb743d2a' });
+    myAmapFun.getWeather({
+      success: function (data) {
+        //成功回调
+        console.log(data)
+        that.setData({
+          weatherData: data.liveData
+        })
+      },
+      fail: function (info) {
+        //失败回调
+        console.log(info)
+      }
+    })
   }
 })
